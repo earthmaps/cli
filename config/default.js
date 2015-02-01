5e2d354cd2893b41b14238e176b456292748a87e
Makis Tracend <makis.tracend@gmail.com>
2015-02-01 01:35:53 -0800

    scaffolding

---
+++ config/default.js
@@ -0,0 +1,5 @@
+module.exports = {
+	key: "{{KEY}}",
+	secret: "{{SECRET}}",
+	url: "http://earthmaps.org"
+};
---
+++ examples/setup.sh
@@ -0,0 +1,15 @@
+
+# Installing the Earthmaps CLI
+
+# 1. Install the main executable
+npm install -g earthmaps-cli
+
+#2. Setup Earthmaps (only needed once)
+earthmaps setup
+
+# you'll be propted with two entries
+# - email: ...
+# - token: ...
+
+# 3. Find out API commands using the help
+earthmaps -h
